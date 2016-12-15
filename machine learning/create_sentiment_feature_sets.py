8fff40d4a92622c0e7f6fbf4360595a61df9e50b
Rodrigo <rodrigo_rosatti@hotmail.com>
2016-12-15 21:55:36 -0200

    study neural net with 'real data'

---
+++ machine learning/create_sentiment_feature_sets.py
@@ -0,0 +1,105 @@
+'''
+Ex: lexicon: [chair, table, spoon, television]
+
+Sentence: I pulled the chair up to the table
+
+Converting this sentence to a vector: [1,1,0,0]
+'''
+
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+import numpy as np
+import random
+import pickle
+from collections import Counter
+
+lemmatizer = WordNetLemmatizer()
+hm_lines = 10000000
+
+def create_lexicon(pos, neg):
+	# populate the lexicon with the words in the files pos.txt and neg.txt
+	lexicon = []
+	for fi in [pos,neg]:
+		with open(fi, 'r') as f:
+			contents = f.readlines()
+			for l in contents[:hm_lines]:
+				all_words = word_tokenize(l.lower())
+				lexicon += list(all_words)
+
+	lexicon = [lemmatizer.lemmatize(i) for i in lexicon]
+	w_counts = Counter(lexicon)
+
+	# l2 is the final lexicon
+	# We don't want common words (the, and, of)
+	l2 = []
+	for w in w_counts:
+		if 1000 > w_counts[w] > 50:
+			l2.append(w)
+
+	print(len(l2))
+
+
+	return l2
+
+
+# classifying features sets
+def sample_handling(sample, lexicon, classification):
+	# featureset will be a list of lists
+	featureset = []
+
+	with open(sample, 'r') as f:
+		contents = f.readlines()
+		for l in contents[:hm_lines]:
+			current_words = word_tokenize(l.lower())
+			current_words = [lemmatizer.lemmatize(i) for i in current_words]
+			features = np.zeros(len(lexicon))
+
+			for word in current_words:
+				if word.lower() in lexicon:
+					index_value = lexicon.index(word.lower())
+					features[index_value] += 1
+
+			features = list(features)
+			featureset.append([features, classification])
+
+	return featureset
+
+
+def create_feature_sets_and_labels(pos, neg, test_size=0.1):
+	lexicon = create_lexicon(pos, neg)
+	features = []
+	features += sample_handling('pos.txt', lexicon, [1,0])
+	features += sample_handling('neg.txt', lexicon, [0,1])
+	random.shuffle(features)
+
+	features = np.array(features)
+
+	# default = 10% of the data
+	test_size = int(test_size*len(features))
+
+	'''
+	What [:,0] means:
+
+	Ex: [[5,8], [7,9]]
+	Res: [5,7]
+
+	So, it will take all the elements in the 0s index
+
+	In our case it will be:
+	[[[0,1,0,1,1]], [0,1]], ...]
+	[[features, label]]
+
+	'''
+	train_x = list(features[:,0][:-test_size])
+	train_y = list(features[:,1][:-test_size])
+
+	test_x = list(features[:,0][-test_size:])
+	test_y = list(features[:,1][-test_size:])
+
+	return train_x, train_y, test_x, test_y
+
+if __name__ == '__main__':
+	train_x, train_y, test_x, test_y = create_feature_sets_and_labels('pos.txt', 'neg.txt', test_size=0.1)
+	with open('sentiment_set.pickle', 'wb') as f:
+		pickle.dump([train_x, train_y, test_x, test_y], f)
--- machine learning/neural_net.py
+++ machine learning/neural_net.py
@@ -41,29 +41,60 @@ def neural_network_model(data):
 	# ... (input data * weights) + biases
 	# Advantage of using a biases: it will be useful in cases where the input is 0.
 	hidden_1_layer = {'weights': tf.Variable(tf.random_normal([784, n_nodes_hl1])), 
-						'biases':tf.Variable(rf.random_normal(n_nodes_hl1))}
+						'biases':tf.Variable(tf.random_normal([n_nodes_hl1]))}
 
 	hidden_2_layer = {'weights': tf.Variable(tf.random_normal([n_nodes_hl1, n_nodes_hl2])), 
-						'biases':tf.Variable(rf.random_normal(n_nodes_hl2))}
+						'biases':tf.Variable(tf.random_normal([n_nodes_hl2]))}
 	
 	hidden_3_layer = {'weights': tf.Variable(tf.random_normal([n_nodes_hl2, n_nodes_hl3])), 
-						'biases':tf.Variable(rf.random_normal(n_nodes_hl3))}
+						'biases':tf.Variable(tf.random_normal([n_nodes_hl3]))}
 
 	output_layer = {'weights': tf.Variable(tf.random_normal([n_nodes_hl3, n_classes])), 
-						'biases':tf.Variable(rf.random_normal(n_classes))}
+						'biases':tf.Variable(tf.random_normal([n_classes]))}
 
 	# (input data * weights) + biases
 
-	l1 = tf.add(tf.matmul(data, hidden_1_layer['weights']) + hidden_1_layer['biases'])
+	l1 = tf.add(tf.matmul(data, hidden_1_layer['weights']), hidden_1_layer['biases'])
 	# activation function
 	l1 = tf.nn.relu(l1)
 
-	l2 = tf.add(tf.matmul(l1, hidden_2_layer['weights']) + hidden_2_layer['biases'])
+	l2 = tf.add(tf.matmul(l1, hidden_2_layer['weights']), hidden_2_layer['biases'])
 	l2 = tf.nn.relu(l2)
 
-	l3 = tf.add(tf.matmul(l2, hidden_3_layer['weights']) + hidden_3_layer['biases'])
+	l3 = tf.add(tf.matmul(l2, hidden_3_layer['weights']), hidden_3_layer['biases'])
 	l3 = tf.nn.relu(l3)
 
 	output = tf.matmul(l3, output_layer['weights']) + output_layer['biases']
 
 	return output
+
+def train_neural_network(x):
+	# 'One-Hot' format
+	prediction = neural_network_model(x)
+	# it will calculate the difference between the prediction we got to the no label that we have
+	cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(prediction,y))
+
+	# learning_rate = 0.001 (default)
+	optmizer = tf.train.AdamOptimizer().minimize(cost)
+
+	# cycles feed forward + backprop
+	hm_epochs = 10
+
+	with tf.Session() as sess:
+		sess.run(tf.initialize_all_variables())
+
+		# training the network
+		for epoch in range(hm_epochs):
+			epoch_loss = 0
+			for _ in range(int(mnist.train.num_examples/batch_size)):
+				epoch_x, epoch_y = mnist.train.next_batch(batch_size)
+				# optimize the cost passing the xs and ys
+				_, c = sess.run([optmizer, cost], feed_dict={x: epoch_x, y: epoch_y})
+				epoch_loss += c
+			print('Epoch', epoch, 'completed out of', hm_epochs, 'loss:', epoch_loss)
+
+		correct = tf.equal(tf.argmax(prediction,1), tf.argmax(y,1))
+		accuracy = tf.reduce_mean(tf.cast(correct, 'float'))
+		print('Accuracy:', accuracy.eval({x:mnist.test.images, y:mnist.test.labels}))
+
+train_neural_network(x)
